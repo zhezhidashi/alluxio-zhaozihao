471d44195e09c66940e0e8ed3f1f59dada65c023
zhaozihao <zihao.zhao@alluxio.com>
2023-07-31 15:31:33 +0800

    Add Dx Hash

--- dora/core/client/fs/src/test/java/alluxio/client/file/dora/JumpConsistentHashPolicyTest.java
+++ dora/core/client/fs/src/test/java/alluxio/client/file/dora/JumpConsistentHashPolicyTest.java
@@ -18,7 +18,9 @@ public class JumpConsistentHashPolicyTest {
   private final int NUM_WORKERS = 10;
   private final int RemoveWorkerIndex = 9;
   private final int NUM_VIRTUAL_NODES = 1000;
-  private final int NUM_FILES = 100000;
+  private final int NUM_FILES = 2000000;
+  // 注：(NUM_REPLICATION + 1) * NUM_WORKERS 才是 Dx Hash 的"虚拟节点"数
+  private final int NUM_REPLICATION = 99;
   private List<BlockWorkerInfo> mBlockWorkerInfos = new ArrayList<>();
   private List<String> mFileIdList = new ArrayList<>();
   @Before
@@ -66,44 +68,44 @@ public class JumpConsistentHashPolicyTest {
 //      System.out.println(worker.getNetAddress().getHost() + " " + workerCount.get(worker));
 //    }
 
-    // 移除一个worker
-    mBlockWorkerInfos.remove(RemoveWorkerIndex);
-
-    // 重新计算
-    ConsistentHashPolicy newConsistentHashPolicy = new ConsistentHashPolicy(conf);
-    workerCount.clear();
-    List<BlockWorkerInfo> newFileWorkerList = new ArrayList<>();
-    for(String fileId : mFileIdList) {
-      List<BlockWorkerInfo> workers = newConsistentHashPolicy.getPreferredWorkers(mBlockWorkerInfos, fileId, 1);
-      for(BlockWorkerInfo worker : workers) {
-        if(workerCount.containsKey(worker)) {
-          workerCount.put(worker, workerCount.get(worker) + 1);
-        } else {
-          workerCount.put(worker, 1);
-        }
-        newFileWorkerList.add(worker);
-      }
-    }
-
-    // 打印 workerCount
-//    for(BlockWorkerInfo worker : workerCount.keySet()) {
-//      System.out.println(worker.getNetAddress().getHost() + " " + workerCount.get(worker));
+//    // 移除一个worker
+//    mBlockWorkerInfos.remove(RemoveWorkerIndex);
+//
+//    // 重新计算
+//    ConsistentHashPolicy newConsistentHashPolicy = new ConsistentHashPolicy(conf);
+//    workerCount.clear();
+//    List<BlockWorkerInfo> newFileWorkerList = new ArrayList<>();
+//    for(String fileId : mFileIdList) {
+//      List<BlockWorkerInfo> workers = newConsistentHashPolicy.getPreferredWorkers(mBlockWorkerInfos, fileId, 1);
+//      for(BlockWorkerInfo worker : workers) {
+//        if(workerCount.containsKey(worker)) {
+//          workerCount.put(worker, workerCount.get(worker) + 1);
+//        } else {
+//          workerCount.put(worker, 1);
+//        }
+//        newFileWorkerList.add(worker);
+//      }
 //    }
-
-    // 对比有多少个文件存储的Worker发生了变化
-    int count = 0;
-    for(int i = 0; i < FileWorkerList.size(); i++) {
-      if(FileWorkerList.get(i) != newFileWorkerList.get(i)) {
-        count++;
-      }
-    }
-    System.out.println("ConsistentHashPolicy change: " + count);
-
-    // 重新计算方差
-    workerCountList.clear();
-    workerCountList.addAll(workerCount.values());
-    variance = getVariance(workerCountList);
-    System.out.println("ConsistentHashPolicy variance: " + variance);
+//
+//    // 打印 workerCount
+////    for(BlockWorkerInfo worker : workerCount.keySet()) {
+////      System.out.println(worker.getNetAddress().getHost() + " " + workerCount.get(worker));
+////    }
+//
+//    // 对比有多少个文件存储的Worker发生了变化
+//    int count = 0;
+//    for(int i = 0; i < FileWorkerList.size(); i++) {
+//      if(FileWorkerList.get(i) != newFileWorkerList.get(i)) {
+//        count++;
+//      }
+//    }
+//    System.out.println("ConsistentHashPolicy change: " + count);
+//
+//    // 重新计算方差
+//    workerCountList.clear();
+//    workerCountList.addAll(workerCount.values());
+//    variance = getVariance(workerCountList);
+//    System.out.println("ConsistentHashPolicy variance: " + variance);
   }
 
   @Test
@@ -137,17 +139,82 @@ public class JumpConsistentHashPolicyTest {
 //      System.out.println(worker.getNetAddress().getHost() + " " + workerCount.get(worker));
 //    }
 
+//    // 移除一个worker
+//    mBlockWorkerInfos.set(RemoveWorkerIndex, mBlockWorkerInfos.get(mBlockWorkerInfos.size() - 1));
+//    mBlockWorkerInfos.remove(mBlockWorkerInfos.size() - 1);
+//    // 重新计算
+//    List<BlockWorkerInfo> newFileWorkerList = new ArrayList<>();
+//    workerCount.clear();
+//    JumpConsistentHashPolicy newJumpConsistentHashPolicy = new JumpConsistentHashPolicy();
+//    for(String fileId : mFileIdList) {
+//      List<BlockWorkerInfo> workers = newJumpConsistentHashPolicy.getPreferredWorkers(mBlockWorkerInfos, fileId, 1);
+//      for(BlockWorkerInfo worker : workers) {
+//        if(workerCount.containsKey(worker)) {
+//          workerCount.put(worker, workerCount.get(worker) + 1);
+//        } else {
+//          workerCount.put(worker, 1);
+//        }
+//        newFileWorkerList.add(worker);
+//      }
+//    }
+//
+//    // 对比有多少个文件存储的Worker发生了变化
+//    int count = 0;
+//    for(int i = 0; i < FileWorkerList.size(); i++) {
+//      if(FileWorkerList.get(i) != newFileWorkerList.get(i)) {
+//        count++;
+//      }
+//    }
+//    System.out.println("JumpConsistentHashPolicy change: " + count);
+//
+//    // 重新计算方差
+//    workerCountList.clear();
+//    workerCountList.addAll(workerCount.values());
+//    variance = getVariance(workerCountList);
+//    System.out.println("JumpConsistentHashPolicy variance: " + variance);
+  }
+
+  @Test
+  public void testDxHash() throws ResourceExhaustedException {
+    WorkerLocationPolicy dxHashPolicy = new DxHashPolicy(NUM_REPLICATION);
+    HashMap<BlockWorkerInfo, Integer> workerCount = new HashMap<>();
+
+    // 保存每个文件存储到哪个worker
+    List<BlockWorkerInfo> FileWorkerList = new ArrayList<>();
+
+    for (String fileId : mFileIdList) {
+      List<BlockWorkerInfo> workers = dxHashPolicy.getPreferredWorkers(mBlockWorkerInfos, fileId, 1);
+      for (BlockWorkerInfo worker : workers) {
+        if (workerCount.containsKey(worker)) {
+          workerCount.put(worker, workerCount.get(worker) + 1);
+        } else {
+          workerCount.put(worker, 1);
+        }
+        FileWorkerList.add(worker);
+      }
+    }
+
+    // 存到list里面
+    List<Integer> workerCountList = new ArrayList<>(workerCount.values());
+    // 计算方差
+    double variance = getVariance(workerCountList);
+    System.out.println("DxHashPolicy variance: " + variance);
+
+    // 打印 workerCount
+//    for(BlockWorkerInfo worker : workerCount.keySet()) {
+//      System.out.println(worker.getNetAddress().getHost() + " " + workerCount.get(worker));
+//    }
+
     // 移除一个worker
-    mBlockWorkerInfos.set(RemoveWorkerIndex, mBlockWorkerInfos.get(mBlockWorkerInfos.size() - 1));
-    mBlockWorkerInfos.remove(mBlockWorkerInfos.size() - 1);
+    mBlockWorkerInfos.remove(RemoveWorkerIndex);
     // 重新计算
     List<BlockWorkerInfo> newFileWorkerList = new ArrayList<>();
     workerCount.clear();
-    JumpConsistentHashPolicy newJumpConsistentHashPolicy = new JumpConsistentHashPolicy();
-    for(String fileId : mFileIdList) {
-      List<BlockWorkerInfo> workers = newJumpConsistentHashPolicy.getPreferredWorkers(mBlockWorkerInfos, fileId, 1);
-      for(BlockWorkerInfo worker : workers) {
-        if(workerCount.containsKey(worker)) {
+    DxHashPolicy newDxHashPolicy = new DxHashPolicy(NUM_REPLICATION);
+    for (String fileId : mFileIdList) {
+      List<BlockWorkerInfo> workers = newDxHashPolicy.getPreferredWorkers(mBlockWorkerInfos, fileId, 1);
+      for (BlockWorkerInfo worker : workers) {
+        if (workerCount.containsKey(worker)) {
           workerCount.put(worker, workerCount.get(worker) + 1);
         } else {
           workerCount.put(worker, 1);
@@ -158,18 +225,19 @@ public class JumpConsistentHashPolicyTest {
 
     // 对比有多少个文件存储的Worker发生了变化
     int count = 0;
-    for(int i = 0; i < FileWorkerList.size(); i++) {
-      if(FileWorkerList.get(i) != newFileWorkerList.get(i)) {
+    for (int i = 0; i < FileWorkerList.size(); i++) {
+      if (FileWorkerList.get(i) != newFileWorkerList.get(i)) {
         count++;
       }
     }
-    System.out.println("JumpConsistentHashPolicy change: " + count);
+    System.out.println("DxHashPolicy change: " + count);
 
     // 重新计算方差
     workerCountList.clear();
     workerCountList.addAll(workerCount.values());
     variance = getVariance(workerCountList);
-    System.out.println("JumpConsistentHashPolicy variance: " + variance);
+    System.out.println("DxHashPolicy variance: " + variance);
+
   }
 
   // 计算方差
